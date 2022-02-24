58f0a87507433afd6e6b61bca8058fb8dab00e57
Oscar Silva <oscarsilva2356@gmail.com>
2022-02-23 22:20:02 -0800

    Created user model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validateEmail, "Please fill a valid email address"],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/],
+    },
+    thought: {
+      id: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "thought",
+        },
+      ],
+    },
+    friends: {
+      id: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "user",
+        },
+      ],
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", postSchema);
+
+module.exports = User;
