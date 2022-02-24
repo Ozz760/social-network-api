df3e45cdc2b7619b14b7071f384dea6bd239b760
Oscar Silva <oscarsilva2356@gmail.com>
2022-02-23 20:38:39 -0800

    Add inital routers

---
+++ routes/api/thoughtsRouter.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+
+// add routes for thoughts 
+module.exports = router;
---
+++ routes/api/usersRouter.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+
+// add routes for users
+module.exports = router;
