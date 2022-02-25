5c557c2f4c3969d0ad59bca6bcd059e06ebfc329
Oscar Silva <oscarsilva2356@gmail.com>
2022-02-24 23:45:49 -0800

    Finished the usersController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,10 +1,93 @@
 // export functions for handling user requests
-const mongoose = require("mongoose");
 const { Users } = require("../models");
 
 module.exports = {
-  getUsers(req, res) {
+  // Get all users
+  getAllUsers(req, res) {
     Users.find()
+      .then(async (user) => {
+        return res.json(user);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  //Gets a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(400).json(err);
+      });
+  },
+
+  // Create users
+  createUser(req, res) {
+    User.create(req.body)
+      .then(async (user) => {
+        return res.json(user);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // Update a single user
+  updateUser(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, req.body, { new: true })
+      .then(async (user) => {
+        return res.json(user);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // Delete user
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then(async (user) => {
+        return res.json(user);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // Create user friends
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.body.friendId } },
+      { new: true }
+    )
+      .then(async (user) => {
+        return res.json(user);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // Delete a user friend
+  removeFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.body.friendId } },
+      { new: true }
+    )
       .then(async (user) => {
         return res.json(user);
       })
