0f6f588e4f6b3206ecaf8efd14aa05249f5140a8
Oscar Silva <oscarsilva2356@gmail.com>
2022-02-25 00:08:41 -0800

    Finished the thoughtController

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,1 +1,101 @@
-// export function for handling thought request 
+// export function for handling thought request
+const { Thought } = require("../models");
+
+module.exports = {
+  // Get all thoughts
+  getAllThoughts(req, res) {
+    Thought.find()
+      .then(async (thoughts) => {
+        return res.json(thoughts);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  //Gets a single thought
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then(async (thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought with that ID" })
+          : res.json(thought)
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(400).json(err);
+      });
+  },
+
+  // Create a thought
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then(async (thought) => {
+        return res.json(thought);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // Update a single thought
+  updateThought(req, res) {
+    Thought.findOneAndUpdate({ _id: req.params.thoughtId }, req.body, {
+      new: true,
+    })
+      .then(async (thought) => {
+        return res.json(thought);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // Delete thought
+  deleteThought(req, res) {
+    Thought.findOneAndRemove({ _id: req.params.thoughtId })
+      .then(async (thought) => {
+        return res.json(thought);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // Create reaction
+  createReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { friends: req.body.reactionId } },
+      { new: true }
+    )
+      .then(async (thought) => {
+        return res.json(thought);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // Delete a reaction
+  deleteReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { friends: req.body.reactionId } },
+      { new: true }
+    )
+      .then(async (thought) => {
+        return res.json(thought);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+};
--- routes/api/thoughtsRouter.js
+++ routes/api/thoughtsRouter.js
@@ -1,24 +1,30 @@
 const router = require("express").Router();
 
 const {
-    getAllThoughts,
-    getSingleThought, 
-    createThought, 
-    updateThought,
-    deleteThought,
-    createReaction, 
-    deleteReaction
-} = require("../../controllers/thoughtsController")
+  getAllThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtsController");
 
 // api/thoughts
-router.route("/").get(getAllThoughts).post(createThought); 
+router.route("/").get(getAllThoughts).post(createThought);
 
 // api/thoughts/:thoughtId
-router.route("/:thoughtId").get(getSingleThought).delete(deleteThought).put(updateThought);
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .delete(deleteThought)
+  .put(updateThought);
 
 // api/thoughts/:thoughtId/reactions
-router.route("/:thoughtId/reactions").post(createReaction).delete(deleteReaction);
-
+router
+  .route("/:thoughtId/reactions")
+  .post(createReaction)
+  .delete(deleteReaction);
 
 // add routes for thoughts
 module.exports = router;
